76c8a99e64c95e4c81965d8da5f468703973b7d0
Lama <lamahamadeh1985@hotmail.com>
2016-12-08 11:59:08 +0000

    Update Prime_Number.py

--- Week1-Basics-of-Python-3/Prime_Number.py
+++ Week1-Basics-of-Python-3/Prime_Number.py
@@ -6,7 +6,7 @@ Created on Thu Dec 08 11:38:28 2016
 """
 
 while True: #the program keeps asking me to enter a number until the input is not a number 
-#this step can be excluded if we want to check a single number not a punch of numbers
+#this step can be excluded if we want to check a single number not a whole punch of numbers
 
     number = input ("Please enter a number: ")
     
@@ -16,7 +16,7 @@ while True: #the program keeps asking me to enter a number until the input is no
     #meaning that range (2,number) it is actually range (2,number-1)
         if number % factor == 0: #this is the definition of a prime number
             is_prime = False
-            break #to stop checking and make the program much faster
+            break #to stop checking and make the program runs much faster
         
     if is_prime == True:
         print(str(number) +'  ' + "is a prime number!")
