49d6a6611d90ad41004dfac74720e34d4100c37f
Lama <lamahamadeh1985@hotmail.com>
2017-01-17 16:32:39 +0000

    Add files via upload

---
+++ Week2-Python-Libraries-and-Concepts-Used-in-Research/Dice_Probability.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 16:26:48 2017
+
+@author: lamahamadeh
+
+source:
+-------
+Video 2.4.2: Examples Involving Randomness
+Week 2 Overview/Python Libraries and Concepts Used in Research
+Using python for research
+Harvard
+online course provided by edx.org
+url: https://courses.edx.org/courses/course-v1:HarvardX+PH526x+3T2016/courseware/317ce880d7644d35840b1f734be76b06/391063d8f58242e892efafc9903b36e8/
+"""
+
+
