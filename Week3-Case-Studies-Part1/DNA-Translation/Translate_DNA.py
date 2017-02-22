c615cf36059ead8030b49c767df5f00fb633a9e3
Lama <lamahamadeh1985@hotmail.com>
2017-02-22 09:59:40 +0000

    Update Translate_DNA.py

--- Week3-Case-Studies-Part1/DNA-Translation/Translate_DNA.py
+++ Week3-Case-Studies-Part1/DNA-Translation/Translate_DNA.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Feb 21 20:55:21 2017
-
 @author: Lama Hamadeh
 """
 '''
@@ -25,7 +24,9 @@ is the protein sequence of amino acids translated from this DNA.
 
 #Importing DNA Data Into Python
 #-------------------------------
-print("The DNA Sequence From the NCBI Website (Accession Number NM_207618.2) Is....\n")
+#we can/need enter the studying folder through 'cd' and display the current folder using 'pwd'
+
+print("\nThe DNA Sequence From the NCBI Website (Accession Number NM_207618.2) Is....\n")
 inputfile = "DNA.txt"
 f = open (inputfile, "r") #"r" here refers to reading command
 seq = f.read()
@@ -38,3 +39,79 @@ print(seq) #we can even slice up the sequence to see specific part of the DNA fi
 
 #Translating the DNA Sequence
 #-----------------------------
+#the translation process is essentially a table lookup operation.
+#Python provides a very natural object for dealing with these types of situations.
+#The object is a dictionary.
+#In this case, the key objects are strings, each consisting of three letters, 
+#i.e., condons or nucleotide triples, drawn 
+#from the four letter alphabet (A,G,C,T).
+#The value object is also a string but a string consisting of just one-letter symbol
+#used for different amino acids.
+
+#Defining a dictionary called 'table':
+table = {'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+         'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+         'AAC':'N', 'AAt':'N', 'AAA':'K', 'AAG':'K',
+         'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+         'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+         'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+         'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+         'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+         'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+         'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+         'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+         'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+         'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+         'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+         'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+         'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+
+
+print (table['ATC']) #to see what is the corresponding value for the key ATC.
+
+#STEP1: Check that length of sequence is divisible by 3
+
+#STEP2: Look up each 3-letter string in table and store result.
+
+#STEP3: Continue lookups until reaching end of sequence.
+
+#Define a function that describes the translating process.
+
+def translate(seq):
+    '''DOCSTRING: Translate a string containing a nucleotide sequence into a string containing the corresponding 
+    sequence of amino acids. Nucleotides are translated in triplets using the table dictionary; each 
+    amino acid is encoded with a string of length 1.'''
+    
+    table = {'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+         'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+         'AAC':'N', 'AAt':'N', 'AAA':'K', 'AAG':'K',
+         'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+         'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+         'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+         'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+         'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+         'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+         'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+         'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+         'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+         'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+         'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+         'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+         'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
+    protein = ''
+    if len(seq) % 3 == 0: # Check that length of sequence is divisible by 3
+        for i in range(0, len(seq), 3): #Look up each 3-letter string in table and store result.
+            condon = seq[i : i+3]
+            protein += table[condon]
+                
+    return protein
+
+
+#check the function
+
+print(translate("ATG"))
+
+print(help (translate))#This print the docstring that is included inside the function
+
+
+
