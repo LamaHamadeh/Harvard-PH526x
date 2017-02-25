d7c8e5f01949757561116e9838fd046e54a488c6
Lama <lamahamadeh1985@hotmail.com>
2017-02-25 11:38:17 +0000

    Update Homework_Week3_CaseStudy1.py

--- Week3-Case-Studies-Part1/DNA-Translation/Homework_Week3_CaseStudy1.py
+++ Week3-Case-Studies-Part1/DNA-Translation/Homework_Week3_CaseStudy1.py
@@ -6,6 +6,13 @@ Created on Thu Feb 23 21:46:13 2017
 @author: lamahamadeh
 """
 
+'''
+==============================
+Case Study 1 - Caesar Cipher!
+==============================
+'''
+
+
 #A cipher is a secret code for a language. In this case study, we will explore 
 #a cipher that is reported by contemporary Greek historians to have been used 
 #by Julius Caesar to send secret messages to generals during times of war.
