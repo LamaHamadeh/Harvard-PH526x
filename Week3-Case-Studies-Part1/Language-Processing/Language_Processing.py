1dfde084fb5f56f3af42c3387ae89b785daaceac
Lama <lamahamadeh1985@hotmail.com>
2017-03-04 22:45:39 +0000

    Update Language_Processing.py

--- Week3-Case-Studies-Part1/Language-Processing/Language_Processing.py
+++ Week3-Case-Studies-Part1/Language-Processing/Language_Processing.py
@@ -19,9 +19,9 @@ text = "This is a test text. We're keping this text short to keep things managea
 #-----------
 
 def count_words(text):
-    '''count the number of times each word occurs in text (str). 
+    """count the number of times each word occurs in text (str). 
     Return dictionary where keys are unique words and values are 
-    word counts. skip punctuations'''
+    word counts. skip punctuations"""
     text = text.lower() #lowercase for the counting letters so the function can cont the same words whether it's capatilised or not
     skips = [".", ",", ";", ":", "'", '"'] #skipping all the punctuations to not be counted with the words that come bfore them
     for ch in skips:
@@ -36,6 +36,7 @@ def count_words(text):
 
 print(count_words(text))
 print(len(count_words("This comprehension check is to check for comprehension.")))#first quiz question
+
 #------------------------------------------------------------------------------
  
 #using collections module
@@ -44,9 +45,9 @@ print(len(count_words("This comprehension check is to check for comprehension.")
 from collections import Counter
 
 def count_words_fast(text):
-    '''count the number of times each word occurs in text (str). 
+    """count the number of times each word occurs in text (str). 
     Return dictionary where keys are unique words and values are 
-    word counts. skip punctuations'''
+    word counts. skip punctuations"""
     text = text.lower() #lowercase for the counting letters so the function can cont the same words whether it's capatilised or not
     skips = [".", ",", ";", ":", "'", '"'] #skipping all the punctuations to not be counted with the words that come bfore them
     for ch in skips:
@@ -63,25 +64,59 @@ print(count_words(text) is count_words_fast(text))#second quiz question
 
 #read a book
 #-------------
-'''
+
 def read_book(title_path):
-    '''
-    Read a book and return it as a string
-    '''
-    with open(title_path, "r", encoding= "utf8") as current_file:#utf8 is a character encoder 
+    """Read a book and return it as a string"""
+    with open(title_path, "r", encoding= "utf8") as current_file:
         text = current_file.read()
         text = text.replace("\n","").replace("\r","")
     return text
         
 
-text = read_book(' ')#read a book from its path
+text = read_book('/Users/lamahamadeh/Desktop/Introduction.txt.rtf')#read a book from its path
 
 print(len(text))#number of charatcers in the book
 
 #if there is a famous/wanted line in the book we can use the 'find' method to find it
-ind = text.find(" write the famous line")
+ind = text.find("The purpose of this chapter")
 print(ind)#print the index number of the famous/wanted sentence
 sample_text = text[ind : ind + 1000]#slice the paragraph that contains the famous line
-print(sample_text)#print the whole paragraph
-'''  
+print(sample_text)#print the whole chosen paragraph
+ 
+
 #------------------------------------------------------------------------------
+
+#Counting the number of unique words
+#------------------------------------
+
+def word_stats(word_counts):
+   """return the number of unique words and word frequencies"""
+   num_unique = len(word_counts) #calculate the number of unique words in the text
+   counts = word_counts.values() #calculate the frequency of each word in the text
+   return(num_unique,counts)
+    
+    
+text = read_book('/Users/lamahamadeh/Desktop/Introduction.txt.rtf')
+
+word_counts = count_words(text)
+
+(num_unique, counts) = word_stats(word_counts)
+
+print(num_unique) #print the number of unique number of words in the text
+print(sum(counts)) #print the total number of words in the text
+
+#------------------------------------------------------------------------------
+
+#Reading multiple files
+#-----------------------
+ 
+    
+
+
+
+
+
+
+
+
+
