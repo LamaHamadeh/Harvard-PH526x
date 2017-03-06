cdc4d42e38c957b4f0d2fbec0d27cadfd1839b45
Lama <lamahamadeh1985@hotmail.com>
2017-03-06 14:10:50 +0000

    Update Homework_Week3_CaseStudy2.py

--- Week3-Case-Studies-Part1/Language-Processing/Homework_Week3_CaseStudy2.py
+++ Week3-Case-Studies-Part1/Language-Processing/Homework_Week3_CaseStudy2.py
@@ -18,6 +18,37 @@ Case Study 2 -
 #For these exercises, functions count_words_fast, read_book, and word_stats are already defined as in the Case 2 Videos (Videos 3.2.x).
 #----------------------------------------------------------------------------------------------------------------
 
+#Define functions
+#------------------
+from collections import Counter
+
+def count_words_fast(text):
+    """count the number of times each word occurs in text (str). 
+    Return dictionary where keys are unique words and values are 
+    word counts. skip punctuations"""
+    text = text.lower() #lowercase for the counting letters so the function can cont the same words whether it's capatilised or not
+    skips = [".", ",", ";", ":", "'", '"'] #skipping all the punctuations to not be counted with the words that come bfore them
+    for ch in skips:
+        text = text.replace(ch,"")
+    word_counts = Counter(text.split(" "))
+    return word_counts
+
+#------------------
+def read_book(title_path):
+    """Read a book and return it as a string"""
+    with open(title_path, "r") as current_file:
+        text = current_file.read()
+        text = text.replace("\n","").replace("\r","")
+    return text
+
+#------------------
+def word_stats(word_counts):
+   """return the number of unique words and word frequencies"""
+   num_unique = len(word_counts) #calculate the number of unique words in the text
+   counts = word_counts.values() #calculate the frequency of each word in the text
+   return(num_unique,counts)
+
+#----------------------------------------------------------------------------------------------------------------
 
 # Exercise 1
 #-----------
