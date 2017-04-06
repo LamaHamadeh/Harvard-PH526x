f80dcab3310ac479caca172aaf3b6d5e6a61edfe
Lama <lamahamadeh1985@hotmail.com>
2017-04-06 19:58:07 +0100

    Update Bird_Migration.py

--- Week4-Case-Studies-Part2/Bird-Migration/Bird_Migration.py
+++ Week4-Case-Studies-Part2/Bird-Migration/Bird_Migration.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Apr 05 10:31:34 2017
-
-@author: Admin
+@author: Lama Hamadeh
 """
 
 
@@ -12,7 +11,7 @@ Created on Wed Apr 05 10:31:34 2017
 #load the dataframe
 import pandas as pd 
 
-birddata = pd.read_csv('bird_tracking.csv')
+birddata = pd.read_csv('/Users/Admin/Desktop/bird_tracking.csv')
 
 print(birddata.head(5))
 print(birddata.shape)#61920x8
@@ -73,7 +72,7 @@ speed = birddata.speed_2d[birddata.bird_name == 'Eric']
 ind = np.isnan(speed)
 plt.hist(speed[~ind], bins = np.linspace(0, 30, 20), normed = True)
 plt.xlabel('2D speed (m/s)')
-plt.ylabel('Frequency');
+plt.ylabel('Frequency')
 
 
 #ploting histograms using pandas instead of plt
@@ -160,16 +159,32 @@ print(elapsed_time[1000])
 
 plt.plot(np.array(elapsed_time) / datetime.timedelta(days = 1))
 plt.xlabel('Observation')
+plt.ylim((0,300))
 plt.ylabel('Elapsed time (days)')
-
 
 #------------------------------------------------------------------------------
 
 #Calculating Daily Mean Speed
 #-----------------------------
-
-
-
+data = birddata[birddata.bird_name == 'Eric']
+times = data.timestamp
+elapsed_time = [time - times[0] for time in times]
+elapsed_days = np.array(elapsed_time) / datetime.timedelta(days = 1)
+next_day = 1
+inds = []
+daily_mean_speed = []
+for (i,t) in enumerate(elapsed_days):
+    if t < next_day:
+        inds.append(i)
+    else:
+        daily_mean_speed.append(np.mean(data.speed_2d[inds]))
+        next_day += 1
+        inds = []
+        
+plt.figure(figsize = (8,6))
+plt.plot(daily_mean_speed)
+plt.xlabel('Day')
+plt.ylabel('Daily Mean Speed (m/s)')
 
 
 #------------------------------------------------------------------------------
@@ -177,6 +192,24 @@ plt.ylabel('Elapsed time (days)')
 #Using the Cartopy Library
 #-------------------------
 
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+proj = ccrs.Mecrator()
+
+plt.figure(figsize = (10,10))
+ax = plt.axes(projection = proj)
+ax.set_extent((-25.0, 20.0, 52.0, 10.0))
+ax.add_feature(cfeature.LAND)
+ax.add_feature(cfeature.OCEAN)
+ax.add_feature(cfeature.COASTLINE)
+ax.add_feature(cfeature.BORDERS, linestyle = '.')
 
 
+for name in bird_names:
+    ix = birddata['bird_name'] == name
+    x , y = birddata.logitude[ix], birddata.latidute[ix]
+    ax.plot(x,y,'.', transform = ccrs.Geodetic(), label = name)
+    
+plt.legeng(loc = 'upper left')
 #------------------------------------------------------------------------------
