fa4f44087e3c1f3c15c8e5bf1915869d3673c09c
Lama <lamahamadeh1985@hotmail.com>
2017-04-10 12:40:01 +0100

    Add files via upload

---
+++ Week4-Case-Studies-Part2/Social-Network-Analysis/Homework_Week4_CaseStudy3.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 10 12:37 2017
+@author: lamahamadeh
+"""
+
+'''
+================================
+Case Study 3 - Network Homophily
+================================
+'''
+
+#Network homophily occurs when nodes that share an edge share a characteristic 
+#more often than nodes that do not share an edge. In this case study, we will 
+#investigate homophily of several characteristics of individuals connected 
+#in social networks in rural India.
+
+#------------------------------------------------------------------------------
+
+#Exercise 1
+#----------
+
+#individual_characteristics.dta contains several characteristics for each 
+#individual in the dataset such as age, religion, and caste. Use the pandas 
+#library to read in and store these characteristics as a dataframe called df.
+
+
+#Store separate datasets for individuals belonging to Villages 1 and 2 as df1 
+#and df2, respectively. (Note that some attributes may be missing for some 
+#individuals. Here, investigate only those pairs of nodes where the attributes 
+#are known for both nodes. This means that we're effectively assuming that the 
+#data are missing completely at random.)
+
+
+#Use the head method to display the first few entries of df1.
+
+
+#------------------------------------------------------------------------------
+
+#Exercise 2
+#----------
+
+
+
+#------------------------------------------------------------------------------
+
+#Exercise 3
+#----------
+
+
+
+#------------------------------------------------------------------------------
+
+#Exercise 4
+#----------
+
+
+
+
+
+#------------------------------------------------------------------------------
